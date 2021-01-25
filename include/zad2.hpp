8acc4b75e96256f36a89b74376ea016662b8cdf2
bajnit <krzysiek.kallas@me.com>
2021-01-25 18:11:08 +0100

    Update zad2.hpp

--- include/zad2.hpp
+++ include/zad2.hpp
@@ -2,5 +2,35 @@
 
 #include "catch.hpp"
 #include "chrup.hpp"
+using namespace std;
 
 // tutaj klasy Ogorek, Zielony i Kiszony
+
+class Ogorek
+{
+public:
+    virtual std::string chrup()
+
+        std::string jedzOgorek(Ogorek* abc)
+    {
+        if (abc == Zielony)
+        {
+            return "Zielony: " + chrup();
+        }
+        if (abc == Kiszony)
+        {
+            return "Kiszony: " + chrup();
+        }
+    }
+}
+
+class Zielony : public Ogorek
+{
+public:
+    virtual std::string chrup() { return chrupZielony(); }
+}
+
+class Kiszony : public Ogorek
+{
+    virtual std::string chrup() { return chrupKiszony(); }
+}
