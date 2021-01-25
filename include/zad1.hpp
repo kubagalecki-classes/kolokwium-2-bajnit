635542e924d91c8dc6d887062adc7d1e4dc6c63d
bajnit <krzysiek.kallas@me.com>
2021-01-25 17:37:20 +0100

    Update zad1.hpp

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -6,4 +6,33 @@
 #include <sstream>
 #include <string>
 
+using namespace std;
+
 // tutaj klasa Warzywo
+
+class Warzywo
+{
+private:
+    std::string     nazwa;
+    double          cena;
+    int             kolor;
+    static unsigned liczba_warzyw;
+
+public:
+    Warzywo(const std::string& s, double d, int i)
+    {
+        nazwa = s;
+        cena  = d;
+        kolor = i;
+        liczba_warzyw++;
+    }
+
+    unsigned getVeg() { return liczba_warzyw; }
+
+    void opis(std::ostream& stream) const // drukowanie do strumienia
+    {
+        stream << nazwa << ": " << cena << ", " << kolor << endl;
+    }
+
+    ~Warzywo() { liczba_warzyw--; }
+}
