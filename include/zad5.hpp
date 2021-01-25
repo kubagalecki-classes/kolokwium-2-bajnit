55cf4864f7a4f2d4f47c1bcffaac6aab9b93c007
bajnit <krzysiek.kallas@me.com>
2021-01-25 19:10:31 +0100

    Update zad5.hpp

--- include/zad5.hpp
+++ include/zad5.hpp
@@ -3,5 +3,36 @@
 #include <algorithm>
 #include <deque>
 #include <vector>
+using namespace std;
 
 // tutaj algorytm liczKolejneWarzywa
+
+template < typename T >
+bool liczKolejneWarzywa(T a, T b, unsigned n, const std::string s)
+{
+    return search_n(a, b, n, s, );
+}
+
+/*
+first, last
+Forward iterators to the initial and final positions of the searched sequence. The range used is
+[first,last), which contains all the elements between first and last, including the element pointed
+by first but not the element pointed by last.
+
+count
+Minimum number of successive elements to match.
+Size shall be (convertible to) an integral type.
+
+val
+Individual value to be compared, or to be used as argument for pred (in the second version).
+for the first version, T shall be a type supporting comparisons with the elements pointed by
+InputIterator using operator== (with the elements as left-hand size operands, and val as right-hand
+side).
+
+pred
+Binary function that accepts two arguments (one element from the sequence as first, and
+val as second), and returns a value convertible to bool. The value returned indicates whether the
+element is considered a match in the context of this function. The function shall not modify any of
+its arguments.
+
+*/
