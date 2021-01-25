ba9af26e9e06c6fa673b005f229531d9745f4b7e
bajnit <krzysiek.kallas@me.com>
2021-01-25 18:09:00 +0100

    Update zad3.hpp

--- include/zad3.hpp
+++ include/zad3.hpp
@@ -5,3 +5,11 @@
 #include <functional>
 
 // tutaj szablon gotujZupe
+
+template < typename T >
+
+class gotujZupe
+{
+public:
+    unsigned gotujZupe(const Warzywo& w, const T& t) { return t.gotujZupe(w) * t.gotujZupe(w); }
+};
